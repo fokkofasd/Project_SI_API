41f5dccc14e55f4f29330cebcb6617a0039b2eec
tumtpk <pk.prateep@gmail.com>
2018-02-20 11:50:11 +0700

    add create user api

--- ProjectSI_API/Controllers/UserController.cs
+++ ProjectSI_API/Controllers/UserController.cs
@@ -1,19 +1,99 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using ProjectSI_API.DAL;
+using ProjectSI_API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace ProjectSI_API.Controllers
 {
     [Authorize]
-    [RoutePrefix("api/Account")]
-    public class UserController : Controller
+    public class UserController : ApiController
     {
-        // GET: User
-        public ActionResult Index()
+
+        private const string LocalLoginProvider = "Local";
+        private ApplicationUserManager _userManager;
+
+        SIDBEntities _db = new SIDBEntities();
+
+        public ApplicationUserManager UserManager
         {
-            return View();
+            get
+            {
+                return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
         }
+
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+
+        public async Task<IHttpActionResult> create(UserModels model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new ApplicationUser() { UserName = model.Email, Email = model.Email };
+
+            IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                return GetErrorResult(result);
+            }
+
+            DAL.AspNetUser nowUser = _db.AspNetUsers.Where(p => p.Email == user.Email).First();
+            DAL.User gen = new DAL.User();
+            gen.userID = nowUser.Id;
+            gen.personalID = model.personalID;
+            gen.firstname = model.firstname;
+            gen.lastname = model.lastname;
+            gen.nickname = model.nickname;
+            gen.email = model.Email;
+            gen.role = model.role;
+            gen.status = "1";
+
+            _db.Users.Add(gen);
+            _db.SaveChanges();
+
+            return Ok();
+        }
+
     }
 }
---
+++ ProjectSI_API/Models/UserModels.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSI_API.Models
+{
+    public class UserModels
+    {
+        public string personalID { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string nickname { get; set; }
+        public string role { get; set; }
+        public string commander { get; set; }
+        public string status { get; set; }
+        public string userID { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
+        //[DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        //[DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
