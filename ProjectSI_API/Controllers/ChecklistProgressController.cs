e31404cf97994e3ec7246247559a513875fb2b0d
FBK <FBK@DESKTOP-CRD6N65>
2018-03-05 13:25:48 +0700

    Add checklistProogressController and model

---
+++ ProjectSI_API/Controllers/ChecklistProgressController.cs
@@ -0,0 +1,84 @@
+﻿using ProjectSI_API.DAL;
+using ProjectSI_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ProjectSI_API.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/checklistprogress")]
+    public class ChecklistProgressController : ApiController
+    {
+        SIDBEntities _db = new SIDBEntities();
+
+        [Route("create")]
+        public async Task<IHttpActionResult> create(List<ChecklistProgress> model)
+        {
+            Boolean result = true;
+            try
+            {
+                System.Web.HttpContext.Current.Application.Lock();
+                foreach (var checklistProgressModel in model)
+                {
+                    _db.ChecklistProgresses.Add(checklistProgressModel);
+                    _db.SaveChanges();
+                }
+                System.Web.HttpContext.Current.Application.UnLock();
+            }
+            catch (Exception e)
+            {
+                result = false;
+            }
+
+            return Json(new { result = result });
+        }
+
+        [Route("update")]
+        public async Task<IHttpActionResult> update(List<ChecklistProgress> model)
+        {
+            Boolean result = true;
+            try
+            {
+                System.Web.HttpContext.Current.Application.Lock();
+                foreach (var checklistProgressModel in model)
+                {
+                    DAL.ChecklistProgress checklistprogres = _db.ChecklistProgresses.Where(p => p.id == checklistProgressModel.id).FirstOrDefault();
+                    checklistprogres.checklistProgress1 = checklistProgressModel.checklistProgress1;
+                    _db.SaveChanges();
+                }
+                System.Web.HttpContext.Current.Application.UnLock();
+            }
+            catch (Exception e)
+            {
+                result = false;
+            }
+
+            return Json(new { result = result });
+        }
+
+        [Route("getchecklistprogress")]
+        public async Task<IHttpActionResult> getchecklistProgress(ChecklistProgressModel model)
+        {
+            System.Web.HttpContext.Current.Application.Lock();
+            var checklistProgress = from clp in _db.ChecklistProgresses
+                                    where clp.Checklist.goalID.Equals(model.goalID) && 
+                                    clp.userID.Equals(model.userID)
+                                    select
+                         new
+                         {
+                             clp.id,
+                             clp.checklistProgress1,
+                             clp.Checklist.checklistName,
+                             clp.userID,
+                             clp.checklistID
+                         };
+            System.Web.HttpContext.Current.Application.UnLock();
+            return Json(checklistProgress);
+        }
+    }
+}
---
+++ ProjectSI_API/Models/ChecklistProgressModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectSI_API.Models
+{
+    public class ChecklistProgressModel
+    {
+
+        public int id { get; set; }
+
+
+        //[Required(ErrorMessage = "กรุณากรอกชื่อรอบการดำเนินงาน")]
+        [Display(Name = "checklistProgress1")]
+        public string checklistProgress1 { get; set; }
+
+
+        //[Required(ErrorMessage = "กรุณากรอกระยะเวลา (เป็นจำนวนวัน)")]
+        [Display(Name = "checklistID")]
+        public int checklistID { get; set; }
+
+        [Display(Name = "userID")]
+        public int userID { get; set; }
+
+        [Display(Name = "goalID")]
+        public int goalID { get; set; }
+    }
+}
