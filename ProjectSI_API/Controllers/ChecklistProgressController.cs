6147f86b14f327d14f787ed83e7915676f2a0c5a
FBK <FBK@DESKTOP-CRD6N65>
2018-05-03 20:40:10 +0700

    update checklistprogress but not done

--- ProjectSI_API/Controllers/ChecklistProgressController.cs
+++ ProjectSI_API/Controllers/ChecklistProgressController.cs
@@ -76,29 +76,47 @@ namespace ProjectSI_API.Controllers
                          {
                              id = clp.id,
                              checklistProgress1 = clp.checklistProgress1,
-                             checklistID = clp.checklistID
+                             checklistID = clp.checklistID,
+                             goalHandlerId = clp.goalHandlerID
                          };
             System.Web.HttpContext.Current.Application.UnLock();
             return Json(checklistProgresses);
         }
 
         [Route("saveProgress")]
-        public async Task<IHttpActionResult> saveProgress(List<ChecklistProgress> model)
+        public async Task<IHttpActionResult> saveProgress(GoalChecklistModel model)
         {
             Boolean result = true;
             try
             {
-                System.Web.HttpContext.Current.Application.Lock();
 
-                foreach (var checklistProgressModel in model)
+                var preDataClps = from clp in _db.ChecklistProgresses
+                                  where clp.Checklist.goalID.Equals(model.id) && clp.goalHandlerID.Equals(model.goalHandlerId)
+                                  select
+                       new
+                       {
+                           clpId = clp.id
+                       };
+
+                
+                foreach (var preDataClp in preDataClps)
                 {
-                    DAL.ChecklistProgress checklistprogress = _db.ChecklistProgresses.Where(p => p.id == checklistProgressModel.id).FirstOrDefault();
-                    checklistprogress.checklistProgress1 = checklistProgressModel.checklistProgress1;
+                    DAL.ChecklistProgress checklistprogress = _db.ChecklistProgresses.Where(p => p.id == preDataClp.clpId).FirstOrDefault();
+                    foreach (var clp in model.checklistProgresses)
+                    {
+                        checklistprogress.checklistProgress1 = 1;
+                        if (checklistprogress.id == clp.id)
+                        {
+                            checklistprogress.checklistProgress1 = 2;
+                            break;
+                        }
+                    }
                     checklistprogress.time = DateTime.Now;
+                    
                     _db.SaveChanges();
                 }
 
-                System.Web.HttpContext.Current.Application.UnLock();
+
             }
             catch (Exception e)
             {
