1cd07fcf8aa97fe3f2bbee33006accde13a4519c
FBK <FBK@DESKTOP-CRD6N65>
2018-05-03 20:41:56 +0700

    Add file graph model

---
+++ ProjectSI_API/Models/graphCommanderModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSI_API.Models
+{
+    public class graphCommanderModel
+    {
+        public string studentName { get; set; }
+        public float percentAverage { get; set; }
+
+        public graphCommanderModel(string studentName, float percentAverage)
+        {
+            this.studentName = studentName;
+            this.percentAverage = percentAverage;
+        }
+    }
+}
---
+++ ProjectSI_API/Models/graphSelfModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSI_API.Models
+{
+    public class graphSelfModel
+    {
+        public string goalName { get; set; }
+        public float percent { get; set; }
+
+        public graphSelfModel(string goalName, float percent)
+        {
+            this.goalName = goalName;
+            this.percent = percent;
+        }
+    }
+}
